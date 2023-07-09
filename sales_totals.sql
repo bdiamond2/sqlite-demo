fb4209eb645fbf1c56711effd418a005875ae4b5
bdiamond2 <87038022+bdiamond2@users.noreply.github.com>
2023-07-09 13:23:04 -0500

    SalesTotal by employee

---
+++ sales_totals.sql
@@ -0,0 +1,25 @@
+with customer_totals as (
+    select
+        customerid
+        , sum(total) as CustomerTotal
+    from invoices
+    group by customerid
+),
+sales_totals as (
+    select
+        e.employeeid
+        , sum(ct.customertotal) as SalesTotal
+    from customers c
+    left join employees e on e.employeeid = c.supportrepid
+    left join customer_totals ct on c.customerid = ct.customerid
+    group by e.employeeid
+)
+select
+    e.employeeid
+    , e.firstname
+    , e.lastname
+    , e.title
+    , coalesce(st.salestotal, 0) as SalesTotal
+from employees e
+left join sales_totals st on st.employeeid = e.employeeid
+;
