6aa4c81d5fc0befc0dc57c0322278f85096ebc2e
bdiamond2 <87038022+bdiamond2@users.noreply.github.com>
2023-07-11 19:29:13 -0500

    Some comments, added quantity to by country

--- sales_by_country.sql
+++ sales_by_country.sql
@@ -1,9 +1,11 @@
 select
     c.country
-    , sum(i.total) as Revenue
-    , count(distinct i.customerid) as Customers
+    , sum(ii.quantity) as Quantity
+    , sum(ii.unitprice * ii.quantity) as Revenue
+    , count(distinct i.customerid) as UniqueCustomers
 from invoices i
 left join customers c on i.customerid = c.customerid
+left join invoice_items ii on i.invoiceid = ii.invoiceid
 group by c.country
 order by Revenue desc
 ;
